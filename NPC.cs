c82c10d1c3736bec7c61ae3ab09bc367373e2046
BCHDan <danielv9340@gmail.com>
2025-06-27 18:07:04 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour 
+{
+    public int health=5;
+    public float speed = 1.2f;
+    public int level = 1;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
