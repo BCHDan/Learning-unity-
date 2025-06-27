791b141a8146243183ef8b082daa4d93db0ba356
BCHDan <danielv9340@gmail.com>
2025-06-27 18:25:56 +0300

    Add files via upload

--- NPC.cs
+++ NPC.cs
@@ -1,16 +1,24 @@
+using System;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NPC : MonoBehaviour 
 {
     public int health=5;
-    public float speed = 1.2f;
+    public float speed = 0f;
     public int level = 1;
+    bool move;
     void Start()
     {
-
+        move= false;
     }
     void Update()
     {
+        if (move==true)
+        {
+            speed = 1.2;
+        }
         
     }
 }
