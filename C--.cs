3beb0e93de4adf585e8441a0150099d1283e3d16
BCHDan <danielv9340@gmail.com>
2025-06-27 17:27:07 +0300

    Add files via upload

---
+++ C--.cs
@@ -0,0 +1,18 @@
+using System;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class C :MonoBehaviour
+{
+    void setup()
+    {
+        
+        print("read");
+        
+    }
+     void loop()
+    {   
+
+        
+    }
+}
